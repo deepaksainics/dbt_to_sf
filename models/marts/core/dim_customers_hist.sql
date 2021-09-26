005f0ca25c468c02ad3881fcf453bc10b836fa00
deepaksainics@gmail.com <deepaksainics@gmail.com>
2021-09-26 07:43:54 +0000

    Changes

--- models/marts/core/dim_customers_hist.sql
+++ models/marts/core/dim_customers_hist.sql
@@ -11,7 +11,8 @@ with dim_customer_hist as (
     select customer_id,first_name,
         last_name,first_order_date,
         most_recent_order_date, number_of_orders,
-        Test_column , '{{invocation_id}}' 	        		AS 	 			dbt_invocation_id 
+        Test_column         ,
+        '{{invocation_id}}' 	        		AS 	 			dbt_invocation_id 
         from {{ ref('dim_customers') }}
       {% if is_incremental() %}
       where MOST_RECENT_ORDER_DATE >= (select max(MOST_RECENT_ORDER_DATE) from {{ this }})
