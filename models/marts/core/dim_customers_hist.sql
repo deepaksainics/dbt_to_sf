6ba72e8d582be3e280f1a498ab9836337a7994da
deepaksainics@gmail.com <deepaksainics@gmail.com>
2021-09-26 00:12:11 +0000

    Added the tags and audit scripts

--- models/marts/core/dim_customers_hist.sql
+++ models/marts/core/dim_customers_hist.sql
@@ -1,7 +1,12 @@
 {{ config(
+	tags=["persisted"],
     materialized = 'incremental',
+	transient=false,
     unique_key = 'customer_id'
-    ) }}
+	pre_hook = logging.log_model_start_event(),
+    post_hook = [ "{{retail_traffic_nightly_updates()}}"," {{ logging.log_model_end_event() }}" , "{{generate_custom_audit(this)}}" ] 
+    ) 
+}}
 with dim_customer_hist as (
     select * from {{ ref('dim_customers') }}
       {% if is_incremental() %}
--- snapshots/orders_hist.sql
+++ snapshots/orders_hist.sql
@@ -4,7 +4,9 @@
         target_schema=generate_schema_name('MART'),
         unique_key='id',
         strategy='check',
-        check_cols=['status']
+        check_cols=['status'],
+        pre_hook = logging.log_model_start_event(),
+        post_hook = [ "{{retail_traffic_nightly_updates()}}"," {{ logging.log_model_end_event() }}" , "{{generate_custom_audit(this)}}" ] 
         )
 }}
 
