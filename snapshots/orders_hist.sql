49da71c68e442abeb8843e2303ad89867b4cd229
deepaksainics@gmail.com <deepaksainics@gmail.com>
2021-09-26 07:49:04 +0000

    Taken out audit from snapshot

--- snapshots/orders_hist.sql
+++ snapshots/orders_hist.sql
@@ -5,9 +5,7 @@
         unique_key='id',
         strategy='check',
         check_cols=['status'],
-        pre_hook = logging.log_model_start_event(),
-        post_hook = [ "{{retail_traffic_nightly_updates()}}"," {{ logging.log_model_end_event() }}" , "{{generate_custom_audit(this)}}" ] 
-        )
+      )
 }}
 
 select * from {{ source('jaffle_shop', 'orders') }}
